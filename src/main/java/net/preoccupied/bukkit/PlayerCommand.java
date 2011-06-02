425b45791210d63946a621be8e95d8af4b14a27d
Christopher O'Brien <obriencj@gmail.com>
2011-06-02 11:38:18 -0400

    Adding a non-permission version of the simpler command api, PlayerCommand. Moved away from using reflection to dispatch run methods by argument count. Removed useless annotation class.

---
+++ src/main/java/net/preoccupied/bukkit/PlayerCommand.java
@@ -0,0 +1,117 @@
+package net.preoccupied.bukkit;
+
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public abstract class PlayerCommand implements CommandExecutor {
+
+    private PluginCommand command = null;
+
+
+
+    public PlayerCommand(PluginCommand com) {
+	this.command = com;
+	command.setExecutor(this);
+    }
+
+
+
+    public PlayerCommand(JavaPlugin plugin, String name) {
+	this(plugin.getCommand(name));
+    }
+
+
+
+    public void msg(Player p, String m) {
+	if(p != null) p.sendMessage(m);
+    }
+
+
+
+    public void err(Player p, String m) {
+	if(p != null) p.sendMessage("$4" + m + "$f");
+    }
+
+
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	if(! (sender instanceof Player))
+	    return false;
+
+	Player player = (Player) sender;
+	return runUnquoted(player, args);
+    }
+
+
+
+    /** Override this if you don't want command quote parsing to
+	happen */
+    public boolean runUnquoted(Player player, String[] args) {
+	return run(player, CommandUtils.processQuotes(args));
+    }
+
+    
+    
+    /** Override this if you want and quote parsing, but want to be
+	able to deal with a variable number of arguments. If not
+	overridden, it will dispatch to the run with the correct
+	number of string arguments. */
+    public boolean run(Player player, String[] args) {
+	switch(args.length) {
+	case 0:
+	    return run(player);
+	case 1:
+	    return run(player, args[0]);
+	case 2:
+	    return run(player, args[0], args[1]);
+	case 3:
+	    return run(player, args[0], args[1], args[2]);
+	case 4:
+	    return run(player, args[0], args[1], args[2], args[3]);
+	case 5:
+	    return run(player, args[0], args[1], args[2], args[3], args[4]);
+	case 6:
+	    return run(player, args[0], args[1], args[2], args[3], args[4],
+		       args[5]);
+	case 7:
+	    return run(player, args[0], args[1], args[2], args[3], args[4],
+		       args[5], args[6]);
+	case 8:
+	    return run(player, args[0], args[1], args[2], args[3], args[4],
+		       args[5], args[6], args[7]);
+	default:
+	    return false;
+	}
+    }
+
+
+
+    /* these get overridden with public versions when anonymous
+       subclasses of PlayerCommand are created */
+
+    public boolean run(Player p) { return false; }
+    public boolean run(Player p, String a) { return false; }
+    public boolean run(Player p, String a, String b) { return false; }
+    public boolean run(Player p, String a, String b, String c) { return false; }
+    public boolean run(Player p, String a, String b, String c, String d) { return false; }
+    public boolean run(Player p, String a, String b, String c, String d,
+		       String e) { return false; }
+    public boolean run(Player p, String a, String b, String c, String d,
+		       String e, String f) { return false; }
+    public boolean run(Player p, String a, String b, String c, String d,
+		       String e, String f, String g) { return false; }
+    public boolean run(Player p, String a, String b, String c, String d,
+		       String e, String f, String g, String h) { return false; }
+
+
+}
+
+
+/* The end. */
--- src/main/java/net/preoccupied/bukkit/permissions/PermissionCommand.java
+++ src/main/java/net/preoccupied/bukkit/permissions/PermissionCommand.java
@@ -1,37 +1,30 @@
 package net.preoccupied.bukkit.permissions;
 
 
-import java.lang.reflect.Method;
-
 import java.util.Map;
-import java.util.TreeMap;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.config.Configuration;
 
 import net.preoccupied.bukkit.CommandUtils;
+import net.preoccupied.bukkit.PlayerCommand;
 import net.preoccupied.bukkit.permissions.PermissionCheck;
 
 
 
-public abstract class PermissionCommand implements CommandExecutor {
-    
-    private Map<Integer,PermissionCommandForm> forms;
+public abstract class PermissionCommand extends PlayerCommand {
 
-    private PluginCommand command = null;
     
     private String permission = null;
     private PermissionCheck permcheck = null;
 
 
+
     public PermissionCommand(PluginCommand com) {
-	this.forms = new TreeMap<Integer,PermissionCommandForm>();
+	super(com);
 
 	PluginDescriptionFile conf = com.getPlugin().getDescription();
 
@@ -40,25 +33,14 @@ public abstract class PermissionCommand implements CommandExecutor {
 	m = (Map) m.get(com.getName());
 
 	this.permission = (String) m.get("permission");
-
-	this.command = com;
-	command.setExecutor(this);
-    }
-
-
-    public PermissionCommand(JavaPlugin plugin, String name) {
-	this(plugin.getCommand(name));
-    }
-
-
-    public void msg(Player p, String m) {
-	p.sendMessage(m);
     }
 
 
 
     public boolean permissionCheck(Player player) {
-	if(permission == null) return true;
+	if(permission == null) {
+	    return true;
+	}
 
 	if(permcheck == null) {
 	    permcheck = PermissionCheck.forNode(permission);
@@ -76,79 +58,13 @@ public abstract class PermissionCommand implements CommandExecutor {
 	Player player = (Player) sender;
 
 	if(! permissionCheck(player)) {
+	    err(player, "You do not have the permission to do that.");
 	    return true;
+
 	} else {
 	    return runUnquoted(player, args);
 	}
     }
-
-
-    /** Override this if you want permission checking but don't want
-	command quote parsing to happen */
-    public boolean runUnquoted(Player player, String[] args) {
-	return run(player, CommandUtils.processQuotes(args));
-    }
-    
-    
-    /** Override this if you want permission checking and quote
-	parsing, but want to be able to deal with a variable number of
-	arguments */
-    public boolean run(Player player, String[] args) {
-    	PermissionCommandForm form = findCommandForm(args.length);
-	return (form != null && form.invoke(this, player, args));
-    }
-
-
-    /* originally wrote this intending to find methods that existed
-       only on the anonymous inner class, but then I discovered that I
-       couldn't call those via reflection. I could probably tear all
-       of the reflection code out and just switch which method to call
-       by the argument count. Really disappointed with that, since we
-       are now limited in how many arguments we can take by how many
-       private run methods I put in this class. */
-    
-
-    private PermissionCommandForm findCommandForm(int count) {
-	PermissionCommandForm pcf = forms.get(count);
-	if(pcf == null) {
-	    Method m = findRunMethod(count);
-	    if(m != null) {
-		pcf = new PermissionCommandForm(m, permission);
-		forms.put(count, pcf);
-	    }
-	}
-	return pcf;
-    }
-
-
-    private Method findRunMethod(int count) {
-	Class<?>[] sig = new Class<?>[count + 1];
-	sig[0] = Player.class;
-	while(count > 0) sig[count--] = String.class;
-
-	Method m = null;
-	try {
-	    m = PermissionCommand.class.getMethod("run", sig);
-
-	} catch(NoSuchMethodException e) {
-	    System.out.println(e);
-	    m = null;
-	}
-
-	return m;
-    }
-
-
-    /* these get overridden with public versions when anonymous
-       subclasses of PermissionCommand are created */
-
-    public boolean run(Player p) { return false; }
-    public boolean run(Player p, String a) { return false; }
-    public boolean run(Player p, String a, String b) { return false; }
-    public boolean run(Player p, String a, String b, String c) { return false; }
-    public boolean run(Player p, String a, String b, String c, String d) { return false; }
-    public boolean run(Player p, String a, String b, String c, String d, String e) { return false; }
-    public boolean run(Player p, String a, String b, String c, String d, String e, String f) { return false; }
 
 
 }
