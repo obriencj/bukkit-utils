f1bc3648125b89dfcda9552666606a72ddf00815
Christopher O'Brien <obriencj@gmail.com>
2011-08-16 19:22:49 -0400

    Changing to the built-in permission API

--- src/main/java/net/preoccupied/bukkit/permissions/PermissionCheck.java
+++ src/main/java/net/preoccupied/bukkit/permissions/PermissionCheck.java
@@ -4,13 +4,7 @@ package net.preoccupied.bukkit.permissions;
 import java.util.Map;
 import java.util.HashMap;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
-
-import com.nijiko.permissions.PermissionHandler;
-import com.nijikokun.bukkit.Permissions.Permissions;
 
 
 /**
@@ -22,7 +16,6 @@ import com.nijikokun.bukkit.Permissions.Permissions;
 public class PermissionCheck {
 
     private final String node;
-    private static PermissionHandler handler = null;
     private static Map<String,PermissionCheck> cache = new HashMap<String,PermissionCheck>();
 
 
@@ -31,22 +24,8 @@ public class PermissionCheck {
     }
 
 
-    private static PermissionHandler getPermissionHandler() {
-	if(handler == null) {
-	    synchronized(PermissionCheck.class) {
-		if(handler == null) {
-		    PluginManager pm = Bukkit.getServer().getPluginManager();
-		    Plugin plug = pm.getPlugin("Permissions");
-		    handler = ((Permissions) plug).getHandler();
-		}
-	    }
-	}
-	return handler;
-    }
-
-
     public boolean check(Player player) {
-	return getPermissionHandler().has(player, this.node);
+	return player.hasPermission(node);
     }
 
 
